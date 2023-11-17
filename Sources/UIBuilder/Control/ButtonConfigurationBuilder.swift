089d528dd3abfb3a49ca761651976bc823026934
hyun subin <sunny5875@hanyang.ac.kr>
2023-11-17 13:58:12 +0900

    delete setBackgroundCornerRadius

--- Sources/UIBuilder/Control/ButtonConfigurationBuilder.swift
+++ Sources/UIBuilder/Control/ButtonConfigurationBuilder.swift
@@ -31,10 +31,6 @@ public class ButtonConfigurationBuilder {
         return self
     }
     
-    public func setBackgroundCornerRadius(_ radius: CGFloat) -> Self {
-        configuration.background.cornerRadius = radius
-        return self
-    }
     
     public func setBackground(_ backgroundClosure: (() -> UIBackgroundConfiguration) ) -> Self {
         configuration.background = backgroundClosure()
